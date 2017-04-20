cc0a4ae1d5e0250f45e2dbce4a381eb4a1b279b0
Marcus Ng <marcusng@Marcuss-MacBook-Pro.local>
2017-04-20 10:54:50 -0400

    ArrayPriorityQueue

---
+++ PriorityQueue.java
@@ -0,0 +1,16 @@
+// SzechuanSauce
+// Marcus Ng, Thomas Lin, Ish Mahdi
+// HW #32: Getting Past the Velvet Rope
+// 2017-4-19
+
+public interface PriorityQueue<T> {
+
+    public void add(T x);
+
+    public boolean isEmpty();
+
+    public T peekMin();
+
+    public T removeMin();
+    
+}

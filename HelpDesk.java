cd3734034769c04c140084a03e820c2602c22b68
Marcus Ng <marcusng@Marcuss-MacBook-Pro.local>
2017-04-21 10:58:43 -0400

    HelpDesk

---
+++ HelpDesk.java
@@ -0,0 +1,16 @@
+public class HelpDesk {
+
+    private ArrayPriorityQueue<Tickets> tickets;
+    private ArrayList<String> problemKeyWords;
+    private ArrayList<String> solutions;
+
+    // Return a solution that contains the keyword
+    public String giveSolution(String keyword) {
+
+    }
+
+    public static void main() {
+
+    }
+    
+}

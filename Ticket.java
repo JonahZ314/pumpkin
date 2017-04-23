29b886cbad2b1579925f74b65af59a162a603661
Allan Wang <awang14@stuy.edu>
2017-04-22 20:33:03 -0400

    updated addKeywords

--- Ticket.java
+++ Ticket.java
@@ -7,14 +7,22 @@
 import java.util.ArrayList;
 
 public class Ticket implements Comparable {
-    
+
     private int vipLevel;
     private ArrayList keywords;
     private String description;
     private String name;
     private boolean resolved;
     private int ID;
-    
+
+  /*Possible keywords
+      "monitor", "power", "slow",
+      "internet", "printer", "files",
+      "install", "noise", "heat"
+    */
+
+
+
     public Ticket(String newName, String desc, int level) {
 	vipLevel = level;
 	keywords = new ArrayList<String>();
@@ -23,7 +31,7 @@ public class Ticket implements Comparable {
 	resolved = false;
 	ID = (int) System.currentTimeMillis();
     }
-    
+
     public int compareTo(Object hi) {
 	Ticket hey = (Ticket) hi;
 	if (this.vipLevel > hey.vipLevel) {
@@ -36,23 +44,23 @@ public class Ticket implements Comparable {
 	    return 0;
 	}
     }
-    
+
     public int getVIP() {
 	return vipLevel;
     }
-    
+
     public String getDesc() {
 	return description;
     }
-    
+
     public String getName() {
 	return name;
     }
-    
+
     public boolean getStatus() {
 	return resolved;
     }
-    
+
     public int getID() {
 	return ID;
     }
@@ -61,7 +69,7 @@ public class Ticket implements Comparable {
     public void setVIP(int level) {
 	vipLevel = level;
     }
-    
+
     //add information to problem description
     public void addInfo(String info) {
 	description += " " + info;
@@ -69,13 +77,26 @@ public class Ticket implements Comparable {
 
     // Parse through description for keywords
     public void addKeywords(String description){
-	
+	String[] a = description.split("\\ |\\?|\\!|\\.");
+  for (int i=0; i<a.length; i++){
+    if (a[i].equals("monitor") ||
+        a[i].equals("power") ||
+        a[i].equals("slow") ||
+        a[i].equals("internet") ||
+        a[i].equals("printer") ||
+        a[i].equals("files") ||
+        a[i].equals("install")
+        )
+        keywords.add(a[i]);
     }
+  }
 
     // Change status
     public void changeStatus() {
-	resolved = !resolved; 
+	resolved = !resolved;
     }
-    
-    
+
+
+
+
 }
